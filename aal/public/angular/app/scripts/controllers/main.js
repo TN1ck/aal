a3b6fcc0d69ff7832cfaf609a10916853b6f7585
TN1ck <tomwanick@gmail.com>
2013-11-19 20:10:52 +0100

    added todos

--- aal/public/angular/app/scripts/controllers/main.js
+++ aal/public/angular/app/scripts/controllers/main.js
@@ -4,6 +4,16 @@ angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
     $scope.mockup = {name: 'Cillian Murphy', 
                      picture: 'http://hollywoodhatesme.files.wordpress.com/2011/08/cillian-murphy.jpg',
-                     telephone: '023234020'};
+                     telephone: '023234020',
+                     todos: [{type: 'red', text: 'Something something...'},
+                             {type: 'green', text: 'Something something...'},
+                             {type: 'green', text: 'Something something...'},
+                             {type: 'red', text: 'Something something...'},
+                             {type: 'red', text: 'Something something...'},
+                             {type: 'orange', text: 'Something something...'},
+                             {type: 'orange', text: 'Something something...'},
+                             {type: 'red', text: 'Something something...'},
+                             {type: 'red', text: 'Something something...'},
+                             ]};
 
   });
