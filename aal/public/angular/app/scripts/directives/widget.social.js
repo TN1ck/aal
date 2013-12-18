59df6da2615940d2433b690a37b73cad4f410679
TN1ck <tomwanick@gmail.com>
2013-12-18 17:42:23 +0100

    added hangout button and used some sce to show max how easy it is

--- aal/public/angular/app/scripts/directives/widget.social.js
+++ aal/public/angular/app/scripts/directives/widget.social.js
@@ -1,15 +1,22 @@
 'use strict';
 
-/* global angular */
+/* global angular, gapi */
 
 var app = angular.module('angularApp');
 
-app.directive('widgetSocial', function() {
+app.directive('widgetSocial', function($sce) {
     return {
         templateUrl: '/views/templates/widget.social.html',
         restrict: 'E',
         scope: {
             social: '=',
+          },
+          link: function(scope) {
+
+            scope.hangout = function() {
+                gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
+              };
+            scope.htmlForMax = $sce.trustAsHtml('<span style="float: left;" class="btn btn-primary btn-sm">Just for Max</span>');
           }
         };
   });
