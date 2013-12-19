cde892d36a7a0e0d3eafd83e781c0345e2b31856
TN1ck <tomwanick@gmail.com>
2013-12-19 18:54:26 +0100

    removed hangout-button, it was not a good place there and it was not responsive

--- aal/public/angular/app/scripts/directives/widget.social.js
+++ aal/public/angular/app/scripts/directives/widget.social.js
@@ -4,19 +4,18 @@
 
 var app = angular.module('angularApp');
 
-app.directive('widgetSocial', function($sce) {
+app.directive('widgetSocial', function() {
     return {
         templateUrl: '/views/templates/widget.social.html',
         restrict: 'E',
         scope: {
             social: '=',
           },
-          link: function(scope) {
+        link: function(scope) {
 
             scope.hangout = function() {
                 gapi.hangout.render('placeholder-div', { 'render': 'createhangout' });
               };
-            scope.htmlForMax = $sce.trustAsHtml('<span style="float: left;" class="btn btn-primary btn-sm">Just for Max</span>');
           }
         };
   });
