b1c9a49502c7c2cbfa683323b1ac5e45e6b72fb5
conchulio <waltersheridan@gmail.com>
2014-01-03 20:24:00 +0100

    Added settings and a few database configuration options

--- aal/public/angular/app/scripts/controllers/settings.js
+++ aal/public/angular/app/scripts/controllers/settings.js
@@ -4,8 +4,24 @@
 
 var appControllers = angular.module('appControllers');
 
-
 appControllers.controller('SettingsCtrl', ['$scope', '$http',
     function ($scope) {
-}]);
+      $scope.createTestData = function() {
+        $('#create-test-data-button').attr('disabled', 'disabled');
+        $.ajax({url: "/populateDatabase"}).done(function() {
+          // alert("Populated Database with test data");
+        }).always(function() {
+          $('#create-test-data-button').removeAttr('disabled');
+        });
+      };
+      $scope.deleteAllData = function() {
+        $('#delete-all-data-button').attr('disabled', 'disabled');
+        $.ajax({url: "/populateDatabase", type: 'delete'}).done(function() {
+          // alert("Deleted all data from database");
+        }).always(function() {
+          $('#delete-all-data-button').removeAttr('disabled');
+        });
+      };
+    }
+]);
 
