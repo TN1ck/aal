a0c8d396b49ea547dba4396347e2dabbaea21063
TN1ck <tomwanick@gmail.com>
2014-01-03 18:35:49 +0100

    added global menu

--- aal/public/angular/app/scripts/app.js
+++ aal/public/angular/app/scripts/app.js
@@ -5,26 +5,34 @@ var angularApp = angular.module('angularApp', [
     'ngAnimate',
     'angular-flip',
     'appControllers',
-    'google-maps'
-]);
+    'google-maps',
+    'ui.router'
+  ]);
 
-angularApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .when('/toygamer', {
-                templateUrl: 'views/toygamer.html',
-                controller: 'MainCtrl'
-            })
-            .when('/playground', {
-                templateUrl: 'views/playground.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-        }
-]);
+angularApp.config(function($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider
+      .otherwise('/');
+
+    $stateProvider
+      .state('wrapper', {
+        abstract: true,
+        templateUrl: 'views/wrapper.html',
+        controller: 'MainCtrl'
+      })
+      .state('wrapper.main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('wrapper.toygamer', {
+        url: '/toygamer',
+        templateUrl: 'views/toygamer.html',
+        controller: 'MainCtrl'
+      })
+      .state('wrapper.playground',{
+        url: '/playground',
+        templateUrl: 'views/playground.html',
+        controller: 'MainCtrl'
+      });
+  });
