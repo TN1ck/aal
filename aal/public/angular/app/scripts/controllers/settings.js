7340998c44740e601b2ffa9c11c108c00d1b4065
conchulio <waltersheridan@gmail.com>
2014-01-03 18:54:27 +0100

    Forgot files

---
+++ aal/public/angular/app/scripts/controllers/settings.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global angular, moment */
+
+var appControllers = angular.module('appControllers', []);
+
+
+appControllers.controller('SettingsCtrl', ['$scope', '$http',
+    function ($scope) {
+}]);
+
