dea0bd8ded592abf0e8070254197e9a1c96dc202
airkiller <vereintetrottel@online.de>
2014-01-30 15:15:00 +0100

    Fixed: Anzeigebug in der Mobileview. Added: Confirmbutton for MobileID + Agentenkommunikationsspezifikation v2

--- aal/public/angular/app/scripts/controllers/mobile.js
+++ aal/public/angular/app/scripts/controllers/mobile.js
@@ -10,31 +10,46 @@ appControllers.controller('MobileCtrl', ['$scope', 'TextTransmission','$state',
       $scope.foo = '';
       $scope.textinput = '';
 
-      $scope.$watch('mobileIdText', function(newVal, oldVal) {
-        TextTransmission.code = newVal;
-        // TextTransmission.mobileId = newVal;
-        TextTransmission.fetchTextForInputDevice(function(data) {
-        if(data.data === 'wrapper.mobile.todo'){
-          console.log('MobileCtrl received wrapper.mobile.todo');
-          $state.transitionTo('wrapper.mobile.todo');
-        } else if(data.data === 'wrapper.mobile.social'){
-          console.log('MobileCtrl received wrapper.mobile.todo');
-          $state.transitionTo('wrapper.mobile.social');
-        } else {
-          console.log('unknown state received. we received:' + data.data);
-        }
+      // $scope.$watch('mobileIdText', function(newVal, oldVal) {
+      //   TextTransmission.code = newVal;
+      //   // TextTransmission.mobileId = newVal;
+      //   TextTransmission.fetchTextForInputDevice(function(data) {
+      //   if(data.data === 'wrapper.mobile.todo'){
+      //     console.log('MobileCtrl received wrapper.mobile.todo');
+      //     $state.transitionTo('wrapper.mobile.todo');
+      //   } else if(data.data === 'wrapper.mobile.social'){
+      //     console.log('MobileCtrl received wrapper.mobile.todo');
+      //     $state.transitionTo('wrapper.mobile.social');
+      //   } else {
+      //     console.log('unknown state received. we received:' + data.data);
+      //   }
 
-      });
+      // });
         
 
-        console.log("changed code to: "+newVal);
-      });
+      //   console.log("changed code to: "+newVal);
+      // });
 
       $scope.$watch('textinput', function(newVal, oldVal) {
         if (newVal !== oldVal) {
           TextTransmission.deliverTextForWall(newVal);
         }
       });
+
+      $scope.setMobileId = function() {
+        TextTransmission.code = $scope.mobileIdText;
+        TextTransmission.fetchTextForInputDevice(function(data) {
+          if(data.data === 'wrapper.mobile.todo'){
+            console.log('MobileCtrl received wrapper.mobile.todo');
+            $state.transitionTo('wrapper.mobile.todo');
+          } else if(data.data === 'wrapper.mobile.social'){
+            console.log('MobileCtrl received wrapper.mobile.social');
+            $state.transitionTo('wrapper.mobile.social');
+          } else {
+            console.log('unknown state received. we received:' + data.data);
+          }
+        })
+      };
 
       // TextTransmission.fetchText(function(data) {
       //     console.log('got', data.data);
