daa98a131ba6cffb4d17634e417fa7b25c6f7199
TN1ck <tomwanick@gmail.com>
2014-02-01 18:08:58 +0100

    moved ping_pong.xml to conf, jiac is working now

--- aal/app/jiac/src/main/java/jiac/pingpong/PingPongStarter.java
+++ aal/app/jiac/src/main/java/jiac/pingpong/PingPongStarter.java
@@ -4,6 +4,6 @@ import jiac.NodeStarter;
 public class PingPongStarter {
 
 	public static void start() {
-		NodeStarter.startNode("classpath*:ping_pong.xml", "PingPongNode", 5000);
+		NodeStarter.startNode("ping_pong.xml", "PingPongNode", 5000);
 	}
 }
