b7e83c827b9efb5ce68e3dce4d98c6350e5f9c18
TN1ck <tomwanick@gmail.com>
2014-02-02 13:00:45 +0100

    improved menu

--- aal/public/angular/app/scripts/services/radial.service.js
+++ aal/public/angular/app/scripts/services/radial.service.js
@@ -18,14 +18,45 @@ app.factory('RadialService', function() {
         $height = $svg.height(),
         margins = {left: 50, right: 50, top: 50, bottom: 50},
         data = dict.data,
-        transitionTime = 1500;
+        transitionTime = 1000;
 
     svg = svg.append('g');
 
     var oldRects = {};
     var oldText = {};
+    var currentlySelected = 0;
+    var currentLength = 0;
+    var level = 0;
 
-    var drawRects = function(data, level) {
+    var updateRects = function(level) {
+
+      var elHeight = ($height - (margins.top + margins.bottom))/level;
+
+      for (var i = 1; i <= level; i++) {
+        var or = oldRects['.rects-' + i];
+        var ot = oldText['.text-' + i];
+        
+        or.transition()
+          .duration(transitionTime)
+          .attr('height', elHeight)
+          .attr('y', elHeight * (i - 1) + margins.top);
+        
+        if (i === level) {
+          or.style('fill', function(d, j) {
+            return j === currentlySelected ? 'gray' : colors[i % colors.length];
+          });
+        }
+
+        ot.transition()
+          .duration(transitionTime)
+          .attr('y', elHeight * (i - 1) + margins.top + elHeight - 5);
+      }
+    };
+
+    var drawRects = function(data) {
+
+      currentLength = data.length;
+      currentlySelected = currentLength > currentlySelected ? currentlySelected : currentLength - 1;
 
       var rects = svg.selectAll('.rects-new').data(data);
       var text = svg.selectAll('.text-new').data(data);
@@ -49,18 +80,7 @@ app.factory('RadialService', function() {
         .attr('width', x(1) - x(0))
         .attr('height', 0)
         .style('fill', function(d,i) {
-          return colors[(i + level) % colors.length];
-        })
-        .on('click', function(d) {
-          var data = [];
-          var rand = Math.random() * 4 + 2;
-
-          for (var i = 0; i < rand; i++) {
-            data.push({text: 'test'});
-          }
-
-          console.log(data, rand);
-          drawRects(data, level + 1);
+          return i === currentlySelected ? 'gray' : colors[level % colors.length];
         });
 
       text.enter()
@@ -75,23 +95,46 @@ app.factory('RadialService', function() {
         .attr('y', elHeight * level + margins.top + elHeight - 5)
         .style('fill', 'white');
 
-      for (var i = 1; i <= level; i++) {
-        var or = oldRects['.rects-' + i];
-        var ot = oldText['.text-' + i];
-        
-        or.transition()
-          .duration(transitionTime)
-          .attr('height', elHeight)
-          .attr('y', elHeight * (i - 1) + margins.top);
-        
-        ot.transition()
-          .duration(transitionTime)
-          .attr('y', elHeight * (i - 1) + margins.top + elHeight - 5);
-      }
+
+      updateRects(level);
 
     };
+    
+    d3.select('body')
+      .on('keydown', function() {
+      
+      switch (d3.event.keyCode) {
+      
+      case 50:
+        level++;
+        if (level === 1) {
+          drawRects(data);
+        } else {
+          
+          var randData = [];
+          var rand = Math.random() * 4 + 2;
 
-    drawRects(data, 1);
+          for (var i = 0; i < rand; i++) {
+            randData.push({text: 'test'});
+          }
+
+          drawRects(randData);
+        
+        }
+        break;
+      case 49:
+        currentlySelected = currentlySelected > 0 ? currentlySelected - 1 : currentlySelected;
+        updateRects(level);
+        break;
+      case 51:
+        currentlySelected = currentlySelected < (currentLength - 1) ? currentlySelected + 1 : currentlySelected;
+        updateRects(level);
+        break;
+      
+
+      }
+
+    });
 
 
 
