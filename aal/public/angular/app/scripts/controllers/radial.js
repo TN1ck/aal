b7fa2f72ba0cea16b4c5486b1ec466ba5d3969b0
TN1ck <tomwanick@gmail.com>
2014-02-02 17:57:17 +0100

    bla

--- aal/public/angular/app/scripts/controllers/radial.js
+++ aal/public/angular/app/scripts/controllers/radial.js
@@ -7,18 +7,18 @@ var appControllers = angular.module('appControllers');
 
 appControllers.controller('RadialCtrl', function (RadialService) {
 
-  RadialService.drawMenu({
-    selector: '#right',
-    text: 'test', data: [
-      {text: 'test', data: [{text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
-              {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
-              {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
-              {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]}]},
-      {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
-      {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]}]
+  // RadialService.drawMenu({
+  //   selector: '#right',
+  //   text: 'test', data: [
+  //     {text: 'test', data: [{text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
+  //             {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
+  //             {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
+  //             {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]}]},
+  //     {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]},
+  //     {text: 'test', data: [{text: 'test'}, {text: 'test'}, {text: 'test'}]}]
 
 
-  });
+  // });
 
 });
 
