7a6682991a15ab5a101eded45ad75dc5cbc41531
airkiller <vereintetrottel@online.de>
2014-02-08 17:34:30 +0100

    Fixed: Sending Todos is now possible via mobile

--- aal/public/angular/app/scripts/controllers/modal.todo.js
+++ aal/public/angular/app/scripts/controllers/modal.todo.js
@@ -40,63 +40,59 @@
 
 /* global angular */
 
-var app = angular.module('angularApp');
+var appControllers = angular.module('appControllers');
 
-app.factory('ModalTodoCtrl', function () {
-  
-  var createController = function() {
+appControllers.controller('ModalTodoCtrl', function ($scope, $modalInstance, TextTransmission) {
 
-    return function ($scope, $modalInstance, TextTransmission) {
-
-      console.log('I am now in ModalTodoCtrl');
-      $scope.modal = {
-        text: '',
-        type: 'green'
-      };
-
-      TextTransmission.fetchTextForWall(function(data) {
-        if(data.data === 'ok') {
-          $scope.ok();
-        } else if (data.data === 'cancel') {
-          try {
-            $scope.cancel();
-          } catch (e) {
-            
-          }
-        } else {
-          // data.data is an object
-          $scope.modal = data.data;
-        }
-      });
-
-      console.log('We want to send: wrapper.mobile.todo');
-      // TextTransmission.code = TextTransmission.mobileId;
-      // TextTransmission.deliverText('wrapper.mobile.todo');
-      TextTransmission.deliverTextForInputDevice('wrapper.mobile.todo');
-
-      $scope.ok = function () {
-        console.log("ModalTodoCtrl is in ok()");
-        $modalInstance.close();
-        TextTransmission.deliverTextForInputDevice('wrapper.mobile');
-
-        // defered.resolve($scope.modal);
-
-      };
-
-      $scope.cancel = function () {
-        console.log("ModalTodoCtrl is in cancel()");
-        console.log("ModalInstance: " , $modalInstance);
-        $modalInstance.dismiss('cancel');
-        TextTransmission.deliverTextForInputDevice('wrapper.mobile');
-
-        // defered.reject('Canceled');
-
-      };
-    };
+  console.log('I am now in ModalTodoCtrl');
+  $scope.modal = {
+    text: '',
+    type: 'green'
   };
 
-  return {
-    createController: createController
+  TextTransmission.fetchTextForWall(function(data) {
+    console.log('Fetched data in modal.todo.js: ' ,data);
+    if(data.data === 'ok') {
+      try {
+        $scope.ok();
+      } catch (e) {
+
+      }
+    } else if (data.data === 'cancel') {
+      try {
+        $scope.cancel();
+      } catch (e) {
+        
+      }
+    } else {
+      // data.data is an object
+      $scope.modal = data.data;
+    }
+  });
+
+  console.log('We want to send: wrapper.mobile.todo');
+  // TextTransmission.code = TextTransmission.mobileId;
+  // TextTransmission.deliverText('wrapper.mobile.todo');
+  TextTransmission.deliverTextForInputDevice('wrapper.mobile.todo');
+
+  $scope.ok = function () {
+    console.log('ModalTodoCtrl is in ok()');
+    console.log($scope.modal);
+    $modalInstance.close($scope.modal);
+    TextTransmission.deliverTextForInputDevice('wrapper.mobile');
+
+    // defered.resolve($scope.modal);
+
+  };
+
+  $scope.cancel = function () {
+    console.log('ModalTodoCtrl is in cancel()');
+    console.log('ModalInstance: ' , $modalInstance);
+    $modalInstance.dismiss('cancel');
+    TextTransmission.deliverTextForInputDevice('wrapper.mobile');
+
+    // defered.reject('Canceled');
+
   };
 });
 
