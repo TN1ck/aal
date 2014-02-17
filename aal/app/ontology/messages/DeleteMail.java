ecbef7c277d081697aa71c8c972d27a09da390a3
NiklasGebauer <niklas.gebauer@online.de>
2014-02-18 00:40:12 +0100

    fixed and uncommented messages, sorry...

--- aal/app/ontology/messages/DeleteMail.java
+++ aal/app/ontology/messages/DeleteMail.java
@@ -1,25 +1,25 @@
-//package ontology.messages;
-//
-//import ontology.Message;
-//import ontology.MessageType;
-//import MailData.Mail;
-//
-//public class DeleteMail extends Message{
-//
-//    int userID;
-//    Mail mail;
-//
-//    public DeleteMail(String senderID, String receiverID, int userID, Mail mail){
-//    	super(senderID, receiverID, MessageType.DELETE_COMM);
-//    	this.userID = userID;
-//    	this.mail = mail;
-//    }
-//
-//    public int getUserID() {
-//        return userID;
-//    }
-//
-//    public Mail getMail() {
-//    	return mail;
-//    }
-//}
+package ontology.messages;
+
+import ontology.Message;
+import ontology.MessageType;
+import ontology.messages.MailData.Mail;
+
+public class DeleteMail extends Message{
+
+   int userID;
+   Mail mail;
+
+   public DeleteMail(String senderID, String receiverID, int userID, Mail mail){
+   	super(senderID, receiverID, MessageType.DELETE_COMM);
+   	this.userID = userID;
+   	this.mail = mail;
+   }
+
+   public int getUserID() {
+       return userID;
+   }
+
+   public Mail getMail() {
+   	return mail;
+   }
+}
--- aal/app/ontology/messages/SaveTodo.java
+++ aal/app/ontology/messages/SaveTodo.java
@@ -1,25 +1,25 @@
-//package ontology.messages;
-//
-//import ontology.Message;
-//import ontology.MessageType;
-//import TodoData.TodoItem;
-//
-//public class SaveTodo extends Message{
-//
-//    int userID;
-//    TodoItem todo;
-//
-//    public SaveTodo(String senderID, String receiverID, int userID, TodoItem todo){
-//    	super(senderID, receiverID, MessageType.DELETE_INFO);
-//    	this.userID = userID;
-//    	this.todo = todo;
-//    }
-//
-//    public int getUserID() {
-//        return userID;
-//    }
-//
-//    public TodoItem getTodo() {
-//    	return todo;
-//    }
-//}
+package ontology.messages;
+
+import ontology.Message;
+import ontology.MessageType;
+import ontology.messages.TodoData.TodoItem;
+
+public class SaveTodo extends Message{
+
+   int userID;
+   TodoItem todo;
+
+   public SaveTodo(String senderID, String receiverID, int userID, TodoItem todo){
+   	super(senderID, receiverID, MessageType.DELETE_INFO);
+   	this.userID = userID;
+   	this.todo = todo;
+   }
+
+   public int getUserID() {
+       return userID;
+   }
+
+   public TodoItem getTodo() {
+   	return todo;
+   }
+}
