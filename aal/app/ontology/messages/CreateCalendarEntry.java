c194d2c5c1841f360a1ffb6b8a7320d7a44fc03d
TN1ck <tomwanick@gmail.com>
2014-02-18 00:18:12 +0100

    commented messages out

--- aal/app/ontology/messages/CreateCalendarEntry.java
+++ aal/app/ontology/messages/CreateCalendarEntry.java
@@ -1,25 +1,24 @@
-package ontology.messages;
-
-import ontology.Message;
-import ontology.MessageType;
-import CalendarData.Entry;
-
-public class CreateCalendarEntry extends Message{
-
-    int userID;
-    Entry entry;
-
-    public CreateCalendarEntry(String senderID, String receiverID, int userID, Entry entry){
-    	super(senderID, receiverID, MessageType.SAVE_INFO);
-    	this.userID = userID;
-    	this.entry = entry;
-    }
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public Entry getEntry() {
-    	return entry;
-    }
-}
+//package ontology.messages;
+//
+//import ontology.Message;
+//import ontology.MessageType;
+//
+//public class CreateCalendarEntry extends Message{
+//
+//    int userID;
+//    Entry entry;
+//
+//    public CreateCalendarEntry(String senderID, String receiverID, int userID, Entry entry){
+//    	super(senderID, receiverID, MessageType.SAVE_INFO);
+//    	this.userID = userID;
+//    	this.entry = entry;
+//    }
+//
+//    public int getUserID() {
+//        return userID;
+//    }
+//
+//    public Entry getEntry() {
+//    	return entry;
+//    }
+//}
--- aal/app/ontology/messages/DeleteTodo.java
+++ aal/app/ontology/messages/DeleteTodo.java
@@ -1,24 +1,24 @@
-package ontology.messages;
-
-import ontology.Message;
-import ontology.MessageType;
-
-public class DeleteTodo extends Message{
-
-    int userID;
-    int todoID;
-
-    public DeleteTodo(String senderID, String receiverID, int userID, int todoID){
-    	super(senderID, receiverID, MessageType.DELETE_INFO);
-    	this.userID = userID;
-    	this.todoID = todoID;
-    }
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public int getTodoID() {
-    	return todoID;
-    }
-}
+//package ontology.messages;
+//
+//import ontology.Message;
+//import ontology.MessageType;
+//
+//public class DeleteTodo extends Message{
+//
+//    int userID;
+//    int todoID;
+//
+//    public DeleteTodo(String senderID, String receiverID, int userID, int todoID){
+//    	super(senderID, receiverID, MessageType.DELETE_INFO);
+//    	this.userID = userID;
+//    	this.todoID = todoID;
+//    }
+//
+//    public int getUserID() {
+//        return userID;
+//    }
+//
+//    public int getTodoID() {
+//    	return todoID;
+//    }
+//}
