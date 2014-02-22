cce505182cbab4ca7b1c7b948e9b7a474f2c68a6
Jonathan <jseilkopf@gmail.com>
2014-02-22 19:12:31 +0100

    some mail stuff

--- aal/app/controllers/Application.java
+++ aal/app/controllers/Application.java
@@ -5,9 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
 
-import jiac.beans.BeanStarter;
-import jiac.beans.GestureBean;
-import jiac.beans.TodoBean;
+import jiac.beans.*;
 import play.Logger;
 import play.libs.F.Callback;
 import play.libs.F.Callback0;
@@ -17,6 +15,7 @@ import play.mvc.WebSocket;
 import util.ASingleton;
 
 import com.avaje.ebean.annotation.Transactional;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
 //import org.eclipse.jetty.util.log.Log;
@@ -25,160 +24,187 @@ import de.dailab.jiactng.agentcore.AbstractAgentBean;
 //import models.TodoItem;
 
 public class Application extends Controller {
-     
-    public final static HashMap<String, Set<WebSocket.Out<String>>> idsToSockets = ASingleton.idsToSockets;
-    public final static HashMap<WebSocket.In<String>, WebSocket.Out<String>> inToOut = ASingleton.inToOut;
-    public final static LinkedList<WebSocket.Out<String>> outSockets = ASingleton.outSockets;
 
-    public static Result index() {
-        //Logger.info("Starting Jiac");
-        BeanStarter.start();
-        return redirect("index.html");
-    }
-    
-    
-    /*
-     * JIAC COMMUNICATION VIA HTTP STARTS HERE
-     * 
-     */
-    
-    public static Result getTodo(int uid, int id) {
-      String json = "[{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"green\", \"text\": \"bla bla\"},{\"type\": \"green\", \"text\": \"bla bla\"}]";
-      ASingleton.sendData(ASingleton.Sockets.TODO, json);
-        for (AbstractAgentBean agent : ASingleton.agents) {
-            if (agent instanceof TodoBean) {
-            	// currently the agents are not using the correct stuff
-                // ((TodoBean) agent).getTodos(uid, id);
-            }
-        }
-        return ok("ok");
-    }
-    
-    public static Result putTodo(int uid) {
-        return ok("ok");
-    }
-    
-    public static Result deleteTodo(int uid, int id) {
-        return ok("ok");
-    }
-    
-    public static Result getCalendar(int uid, int id) {
-      String json = "[{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"}]";
-      ASingleton.sendData(ASingleton.Sockets.CALENDAR, json);
-        return ok("ok");
-    }
-    
-    public static Result putCalendar(int uid) {
-        return ok("ok");
-    }
-    
-    public static Result deleteCalendar(int uid, int id) {
-        return ok("ok");
-    }
-    
-    public static Result getNews(int uid, int id) {
-      String lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-      String json = "[{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \"" + lorem  + "\"}," + "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \"" + lorem  + "\"}," + "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \"" + lorem  + "\"}, " + "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \"" + lorem  + "\"}," + "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \"" + lorem  + "\"}" + "]";
-      ASingleton.sendData(ASingleton.Sockets.NEWS, json);
-        return ok("ok");
-    }
+	public final static HashMap<String, Set<WebSocket.Out<String>>> idsToSockets = ASingleton.idsToSockets;
+	public final static HashMap<WebSocket.In<String>, WebSocket.Out<String>> inToOut = ASingleton.inToOut;
+	public final static LinkedList<WebSocket.Out<String>> outSockets = ASingleton.outSockets;
 
-    public static Result getMail(int uid, int id) {
-        return ok("ok");
-    }
-    
-    public static Result putMail(int uid) {
-        return ok("ok");
-    }
-    
-    public static Result deleteMail(int uid, int id) {
-        return ok("ok");
-    }
-    
-    public static Result startTraining(int nid) {
-        
-    	for (AbstractAgentBean agent : ASingleton.agents) {
-            if (agent instanceof GestureBean) {
-                ((GestureBean) agent).startTraining(nid);
-            }
-        }
-    	
-        return ok("ok");
-    }
-    
-    public static Result recognize(int nid) {
-    	for (AbstractAgentBean agent : ASingleton.agents) {
-            if (agent instanceof GestureBean) {
-                ((GestureBean) agent).recognize(nid, false);
-            }
-        }
-    	return ok("ok");
-    }
-      
-    @Transactional
-    public static WebSocket<String> websocket() {
-        return new WebSocket<String>() {
-            
-            // Called when the Websocket Handshake is done.
-            public void onReady(final WebSocket.In<String> in, final WebSocket.Out<String> out) {
-    
-                outSockets.add(out); // add to outSockets, this could be changed in the future, used for jiac-stuff
-                inToOut.put(in, out);
+	public static Result index() {
+		// Logger.info("Starting Jiac");
+		BeanStarter.start();
+		return redirect("index.html");
+	}
 
-                in.onMessage(new Callback<String>() {
-                    public void invoke(String event) {
-                        int index = event.indexOf(":");
-                        String prefix = event.substring(0, index);
-                        String message = event.substring(index+1);
+	/*
+	 * JIAC COMMUNICATION VIA HTTP STARTS HERE
+	 */
 
-                        Set<Out<String>> currentSet = idsToSockets.get(prefix);
-                        if (currentSet == null) {
-                            currentSet = new HashSet<Out<String>>();
-                        }
-                        currentSet.add(out);
-                        idsToSockets.put(prefix, currentSet);
-                        
-                        System.out.println("Length of idToSockets: "+idsToSockets.size());
-                        System.out.println("Length of inToOut: "+inToOut.size());
+	public static Result getTodo(int uid, int id) {
+		String json = "[{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"orange\", \"text\": \"bla bla\"},{\"type\": \"red\", \"text\": \"bla bla\"},{\"type\": \"green\", \"text\": \"bla bla\"},{\"type\": \"green\", \"text\": \"bla bla\"}]";
+		ASingleton.sendData(ASingleton.Sockets.TODO, json);
+		for (AbstractAgentBean agent : ASingleton.agents) {
+			if (agent instanceof TodoBean) {
+				// currently the agents are not using the correct stuff
+				// ((TodoBean) agent).getTodos(uid, id);
+			}
+		}
+		return ok("ok");
+	}
 
-                        if (!message.equals("")) {
-                            for (WebSocket.Out<String> outOfSameId : currentSet) {
-                                outOfSameId.write(""+prefix+":"+message);
-                            }
-                        }
-                    }
-                });
-                
-                in.onClose(new Callback0() {
-                    public void invoke() {
-                        WebSocket.Out<String> relatedOut = inToOut.remove(in);
-                        Set<String> keys = idsToSockets.keySet();
-                        
-                        for (String key : keys) {
-                            Set<WebSocket.Out<String>> associatedSockets = idsToSockets.get(key);
-                            associatedSockets.remove(relatedOut);
-                            idsToSockets.put(key, associatedSockets);
-                        }
-                        
-                        System.out.println("Disconnected");
-                    }
-                });
-                
-            }
-        };
-    }
+	public static Result putTodo(int uid) {
+		return ok("ok");
+	}
 
-    @Transactional
-    public static Result startJiac() {
-        Logger.info("Start JIAC");
-        BeanStarter.start();
-        return ok("started jiac");
-    }
+	public static Result deleteTodo(int uid, int id) {
+		return ok("ok");
+	}
 
-    @Transactional
-    public static Result stopJiac() {
-        Logger.info("Stop JIAC");
-        BeanStarter.stop();
-        return ok("stopped jiac");
-    }
+	public static Result getCalendar(int uid, int id) {
+		String json = "[{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"},{\"location\": \"Berlin\", \"startDate\": \"2014-02-29 14:00\", \"startDate\": \"2014-02-29 15:00\", \"text\": \"test test test\"}]";
+		ASingleton.sendData(ASingleton.Sockets.CALENDAR, json);
+		return ok("ok");
+	}
+
+	public static Result putCalendar(int uid) {
+		return ok("ok");
+	}
+
+	public static Result deleteCalendar(int uid, int id) {
+		return ok("ok");
+	}
+
+	public static Result getNews(int uid, int id) {
+		String lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+		String json = "[{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \""
+				+ lorem + "\"},"
+				+ "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \""
+				+ lorem + "\"},"
+				+ "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \""
+				+ lorem + "\"}, "
+				+ "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \""
+				+ lorem + "\"},"
+				+ "{\"header\": \"Lorem ipsum dolor sit.\", \"text\": \""
+				+ lorem + "\"}" + "]";
+		ASingleton.sendData(ASingleton.Sockets.NEWS, json);
+		return ok("ok");
+	}
+
+	public static Result getMail(int uid, int id) {
+		for (AbstractAgentBean agent : ASingleton.agents) {
+			if (agent instanceof MailBean) {
+				((MailBean) agent).getMailData(uid);
+			}
+		}
+		return ok("ok");
+	}
+
+	public static Result putMail(int uid) {
+		JsonNode json = request().body().asJson();
+		if (json == null) {
+			return badRequest("Expecting Json data");
+		} else {
+			for (AbstractAgentBean agent : ASingleton.agents) {
+				if (agent instanceof MailBean) {
+					((MailBean) agent).writeMail(uid,json);
+				}
+			}
+			return ok("ok");
+		}
+	}
+
+	public static Result deleteMail(int uid, int id) {
+		return ok("ok");
+	}
+
+	public static Result startTraining(int nid) {
+
+		for (AbstractAgentBean agent : ASingleton.agents) {
+			if (agent instanceof GestureBean) {
+				((GestureBean) agent).startTraining(nid);
+			}
+		}
+
+		return ok("ok");
+	}
+
+	public static Result recognize(int nid) {
+		for (AbstractAgentBean agent : ASingleton.agents) {
+			if (agent instanceof GestureBean) {
+				((GestureBean) agent).recognize(nid, false);
+			}
+		}
+		return ok("ok");
+	}
+
+	@Transactional
+	public static WebSocket<String> websocket() {
+		return new WebSocket<String>() {
+
+			// Called when the Websocket Handshake is done.
+			public void onReady(final WebSocket.In<String> in,
+					final WebSocket.Out<String> out) {
+
+				outSockets.add(out); // add to outSockets, this could be changed
+										// in the future, used for jiac-stuff
+				inToOut.put(in, out);
+
+				in.onMessage(new Callback<String>() {
+					public void invoke(String event) {
+						int index = event.indexOf(":");
+						String prefix = event.substring(0, index);
+						String message = event.substring(index + 1);
+
+						Set<Out<String>> currentSet = idsToSockets.get(prefix);
+						if (currentSet == null) {
+							currentSet = new HashSet<Out<String>>();
+						}
+						currentSet.add(out);
+						idsToSockets.put(prefix, currentSet);
+
+						System.out.println("Length of idToSockets: "
+								+ idsToSockets.size());
+						System.out.println("Length of inToOut: "
+								+ inToOut.size());
+
+						if (!message.equals("")) {
+							for (WebSocket.Out<String> outOfSameId : currentSet) {
+								outOfSameId.write("" + prefix + ":" + message);
+							}
+						}
+					}
+				});
+
+				in.onClose(new Callback0() {
+					public void invoke() {
+						WebSocket.Out<String> relatedOut = inToOut.remove(in);
+						Set<String> keys = idsToSockets.keySet();
+
+						for (String key : keys) {
+							Set<WebSocket.Out<String>> associatedSockets = idsToSockets
+									.get(key);
+							associatedSockets.remove(relatedOut);
+							idsToSockets.put(key, associatedSockets);
+						}
+
+						System.out.println("Disconnected");
+					}
+				});
+
+			}
+		};
+	}
+
+	@Transactional
+	public static Result startJiac() {
+		Logger.info("Start JIAC");
+		BeanStarter.start();
+		return ok("started jiac");
+	}
+
+	@Transactional
+	public static Result stopJiac() {
+		Logger.info("Stop JIAC");
+		BeanStarter.stop();
+		return ok("stopped jiac");
+	}
 }
--- aal/app/ontology/messages/SendMail.java
+++ aal/app/ontology/messages/SendMail.java
@@ -7,12 +7,14 @@ import ontology.messages.MailData.Mail;
 public class SendMail extends Message{
 
    int userID;
+   String receiver;
    Mail mail;
 
-   public SendMail(String senderID, String receiverID, int userID, Mail mail) {
+   public SendMail(String senderID, String receiverID, int userID, Mail mail, String receiver) {
        super(senderID, receiverID, MessageType.SAVE_COMM);
        this.userID = userID;
        this.mail = mail;
+       this.receiver = receiver;
    }
 
    public int getUserID() {
@@ -21,5 +23,13 @@ public class SendMail extends Message{
 
    public Mail getMail() {
        return mail;
+   }
+   
+   public void setMail(Mail mail) {
+       this.mail = mail;
+   }
+  
+   public String getReceiver() {
+       return receiver;
    }
 }
