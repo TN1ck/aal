44035215b18bb7b71b0488390f9c7020bfb6cab7
Jonathan <jseilkopf@gmail.com>
2014-02-23 14:58:27 +0100

    fix

--- aal/app/jiac/beans/MailBean.java
+++ aal/app/jiac/beans/MailBean.java
@@ -92,7 +92,7 @@ public class MailBean extends AbstractCommunicatingBean {
 				
 				//create new Mail
 				MailData md = new MailData(null,null,0,null);
-				Mail newMail = md.new Mail(subject,content,type,from,received);
+				Mail newMail = md.new Mail(subject,content,type,from,received,0);
 				newMessage.setMail(newMail);
 				JiacMessage message = new JiacMessage(newMessage);
 				// Invoke sendAction
@@ -112,13 +112,13 @@ public class MailBean extends AbstractCommunicatingBean {
 		MailData newMailMessage = new MailData("","",-1,null);
 		ArrayList<Mail> mails = new ArrayList<Mail>();
 		String lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-		mails.add(newMailMessage.new Mail("Could you send me the files?", lorem,"private","jonny", new Date()));
-		mails.add(newMailMessage.new Mail("Dinner tonight", lorem,"private","Wife", new Date()));
-		mails.add(newMailMessage.new Mail("Team Meeting", lorem,"private","Boss", new Date()));
-		mails.add(newMailMessage.new Mail("I'm coming to late", lorem,"private","Max", new Date()));
-		mails.add(newMailMessage.new Mail("Check out this feature", lorem,"private","Tom", new Date()));
-		mails.add(newMailMessage.new Mail("bug report", lorem,"private","Niklas", new Date()));
-		mails.add(newMailMessage.new Mail("easy money", lorem,"private","Nigerian Prince", new Date()));
+		mails.add(newMailMessage.new Mail("Could you send me the files?", lorem,"private","jonny", new Date(),0));
+		mails.add(newMailMessage.new Mail("Dinner tonight", lorem,"private","Wife", new Date(),0));
+		mails.add(newMailMessage.new Mail("Team Meeting", lorem,"private","Boss", new Date(),0));
+		mails.add(newMailMessage.new Mail("I'm coming to late", lorem,"private","Max", new Date(),0));
+		mails.add(newMailMessage.new Mail("Check out this feature", lorem,"private","Tom", new Date(),0));
+		mails.add(newMailMessage.new Mail("bug report", lorem,"private","Niklas", new Date(),0));
+		mails.add(newMailMessage.new Mail("easy money", lorem,"private","Nigerian Prince", new Date(),0));
 		newMailMessage.setMails(mails);
 		receiveMessage(newMailMessage);
 	}
