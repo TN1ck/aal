bef542a14a3f626c28038be60d3bb5c42e69ba16
NiklasGebauer <niklas.gebauer@online.de>
2014-03-03 22:05:50 +0100

    small fix

--- aal/app/util/ASingleton.java
+++ aal/app/util/ASingleton.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
-import org.eclipse.jetty.util.log.Log;
 
 import models.User;
 import play.Logger;
